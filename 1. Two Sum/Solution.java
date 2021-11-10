083cf5f1d5ba47a3460802ff957291e3d66692ad
Islam Azab <islam.3azab@gmail.com>
2021-11-10 20:51:33 +0100

    add Two Sum solution

---
+++ 1. Two Sum/Solution.java
@@ -0,0 +1,23 @@
+// https://leetcode.com/problems/two-sum/
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int n = nums.length;
+        int[] valueDiff = new int[n];
+
+        for(int i=0; i<n; i++) {
+            valueDiff[i] = target - nums[i];
+        }
+
+        for(int i=0; i<n; i++) {
+            int diff = target - nums[i];
+            for(int j=i+1; j<n; j++){
+                if(target == diff + valueDiff[j]) {
+                    return new int[] {i, j};
+                }
+            }
+        }
+
+        return new int[] {0, 1};
+    }
+}
+
